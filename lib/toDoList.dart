f00ce4a6076eee5635af25f3ea7c9b7a5bccaf6b
Thiago Silvério <thiagosilveriosanto@gmail.com>
2022-05-30 13:10:07 -0300

    backup

--- lib/toDoList.dart
+++ lib/toDoList.dart
@@ -9,8 +9,28 @@ class ToDoList extends StatefulWidget {
 
 class _ToDoListState extends State<ToDoList> {
   
-List<String> lista = List[];
+List<String> lista = [];
 final TextEditingController _controllertarefa = TextEditingController();
+
+ @override
+void initState() {
+    super.initState();
+    _controllertarefa.addListener(() {
+      final String text = _controllertarefa.text.toLowerCase();
+      _controllertarefa.value = _controllertarefa.value.copyWith(
+        text: text,
+        selection:
+            TextSelection(baseOffset: text.length, extentOffset: text.length),
+        composing: TextRange.empty,
+      );
+    });
+  }
+
+ @override
+  void dispose() {
+    _controllertarefa.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -26,17 +46,31 @@ final TextEditingController _controllertarefa = TextEditingController();
               controller: _controllertarefa,
               decoration: const InputDecoration(
                 border: OutlineInputBorder(),
-                hintText: 'Adicionar'
+                labelText: 'Adicionar'
               ),
             ),
           ),
           Container( margin: const EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-           child: IconButton(color: Colors.red ,iconSize: 50 ,onPressed: (){}, icon: const Icon(Icons.add_circle)),
+           child: IconButton(color: Colors.red, iconSize: 50 ,onPressed: (){
+             setState(() {
+                lista.add(_controllertarefa.text);
+                _controllertarefa.clear();
+             });
+           }, icon: const Icon(Icons.add_circle)),
             ),
         ],),
-        ListView(children: [
-          
-        ],)
+        Expanded(
+          child: ListView.builder(
+            itemCount: lista.length,
+            itemBuilder: (context, index) {
+               return Card(
+                child: ListTile(
+                  title: Text(lista[index]),
+                ),
+              );
+            },
+          ),
+        )
       ]),
     );
   }
