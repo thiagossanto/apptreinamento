ebe563d72bce2ad08c573221ba5408e90ab02c3c
Thiago Silvério <thiagosilveriosanto@gmail.com>
2022-05-31 11:47:19 -0300

    up

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,5 +1,5 @@
-import 'package:apptreinamento/github.dart';
-import 'package:apptreinamento/toDoList.dart';
+import 'package:apptreinamento/pages/github.dart';
+import 'package:apptreinamento/pages/toDoList.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
